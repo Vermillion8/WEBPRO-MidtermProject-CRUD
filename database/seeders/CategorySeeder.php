800a430bee338629968cf1d5c2dd4539be24ace4
Vermillion8 <5025211101@mhs.its.ac.id>
2023-10-26 01:18:52 +0700

    fix category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,26 +4,42 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $categories = ['quotes', 'food', 'travel', 'fashion', 'general'];
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $categories = $this->getCategories();
 
-        foreach ($categories as $category) {
-            DB::table('categories')->insert([
-                'name' => $category,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+    foreach ($categories as $category) {
+      Category::create([
+        'name' => $category,
+        'created_at' => now(),
+        'updated_at' => now(),
+      ]);
     }
-}
+  }
 
+  /**
+   * Get the categories to be seeded.
+   *
+   * @return array
+   */
+  private function getCategories()
+  {
+    return [
+      'quotes',
+      'food',
+      'travel',
+      'fashion',
+      'general',
+    ];
+  }
+}
+?>
